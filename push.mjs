822e7238de6fe8f73c6491ab1c6b36b506fd496f
Taisuke Fukuno <fukuno@jig.jp>
2020-05-14 11:15:57 +0900

    Initial commit

---
+++ push.mjs
@@ -0,0 +1,200 @@
+import express from 'express'
+import fs from 'fs'
+import multer from 'multer'
+import util from './util.mjs'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const SECPATH = process.env.SECPATH || '/uploaded/'
+const PORT = process.env.PORT || 8006
+
+const app = express()
+
+const getIP = function (req) {
+  if (req.headers['x-forwarded-for']) {
+    return req.headers['x-forwarded-for']
+  }
+  if (req.connection && req.connection.remoteAddress) {
+    return req.connection.remoteAddress
+  }
+  if (req.connection.socket && req.connection.socket.remoteAddress) {
+    return req.connection.socket.remoteAddress
+  }
+  if (req.socket && req.socket.remoteAddress) {
+    return req.socket.remoteAddress
+  }
+  return '0.0.0.0'
+}
+const encodeIP = function (ip) {
+  return ip.replace(/\.|:/g, '_')
+}
+
+const fnid = 'data/id.txt'
+const getID = function () {
+  let id = 0
+  try {
+    id = parseInt(fs.readFileSync(fnid, 'utf-8'))
+  } catch (e) {
+  }
+  id++
+  fs.writeFileSync(fnid, id, 'utf-8')
+  return id
+}
+const getPassCode = function (id) {
+  const fix0 = function (n, m) {
+    const s = '00000000000' + n
+    return s.substring(s.length - m)
+  }
+  const len = 6
+  const n = []
+  for (let i = 0; i < len; i++) {
+    n.push(fix0(util.rnd(10000), 4))
+  }
+  id = parseInt(id)
+  for (;;) {
+    n.push(fix0(id % 10000, 4))
+    id = Math.floor(id / 10000)
+    if (!id) { break }
+  }
+  return n.join('-')
+}
+const decodeID = function (pass) {
+  let id = 0
+  const n = pass.split('-')
+  let m = 1
+  for (let i = 6; i < n.length; i++) {
+    id = parseInt(n[i]) * m
+    m *= 10000
+  }
+  return id
+}
+
+// import bodyParser from 'body-parser'
+// app.use(bodyParser.json())
+// app.post('/', multer({ dest: 'tmp/' }).single('file'), (req, res) => {
+/*
+app.post('/', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Content-Type', 'application/json; charset=utf-8')
+
+  console.log(req)
+  console.log(req.body)
+  console.log(req.url)
+  const id = getID()
+  const pass = getPassCode(id)
+  res.send(JSON.stringify({ res: 'ok', id: id, pass: pass }))
+})
+*/
+app.get(SECPATH + '*', (req, res) => {
+  let fn = req.url.substring(SECPATH.length)
+  const qn = fn.lastIndexOf('?')
+  if (qn >= 0) {
+    fn = fn.substring(0, qn)
+  }
+  console.log(req.url, fn)
+  if (fn.indexOf('..') >= 0) {
+    res.header('Content-Type', 'text/html; charset=utf-8')
+    res.send('err')
+    return
+  }
+  if (fn.length == 0) {
+    const s = []
+    s.push(`<h2>COVID-19</h2>`)
+    try {
+      const list = fs.readdirSync('data/')
+      console.log(list)
+      for (const f of list) {
+        if (f.endsWith('.wav'))
+          s.push(`<a href=${f}>${f}</a>`)
+      }
+    } catch (e) {
+    }
+    s.push('')
+
+    res.header('Content-Type', 'text/html; charset=utf-8')
+    res.send(s.join('<br>'))
+  } else {
+    res.header('Content-Type', 'audio/wav')
+    const data = fs.readFileSync('data/' + fn)
+    res.send(data)
+  }
+})
+app.get('/*', (req, res) => {
+  let url = req.url
+  // console.log(req.query.data)
+  if (req.query.id) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Content-Type', 'application/json; charset=utf-8')
+    const id = parseInt(req.query.id)
+    try {
+      const d = fs.readFileSync('data/' + id + '.json', 'utf-8')
+      res.send(d)
+      return
+    } catch (e) {
+    }
+    res.send(JSON.stringify({ err: 'not found' }))
+    return
+  }
+  const d = req.query.data
+  if (d) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Content-Type', 'application/json; charset=utf-8')
+
+    const data = JSON.parse(d)
+    data.lastUpdate = util.formatYMDHMS()
+    const pass = data.パスコード
+    delete data.パスコード
+    console.log(data, pass)
+    if (pass) {
+      const id = decodeID(pass)
+      const chk = fs.readFileSync('data/' + id + '-pass.txt', 'utf-8')
+      console.log(chk)
+      if (chk === pass) {
+        fs.writeFileSync('data/' + id + '.json', JSON.stringify(data))
+        res.send(JSON.stringify({ res: 'ok', id: id, lastUpdate: data.lastUpdate }))
+        return
+      }
+    }
+
+    // console.log(req)
+    // console.log(req.body)
+    console.log(req.url)
+    const id = getID()
+    const newpass = getPassCode(id)
+    console.log(id, newpass)
+    fs.writeFileSync('data/' + id + '-pass.txt', newpass)
+    fs.writeFileSync('data/' + id + '.json', JSON.stringify(data))
+
+    res.send(JSON.stringify({ res: 'ok', id: id, pass: newpass, lastUpdate: data.lastUpdate }))
+    return
+  }
+  if (url === '/' || url.indexOf('..') >= 0) {
+    url = '/index.html'
+  }
+  let ctype = 'text/plain'
+  if (url.endsWith('.html')) {
+    ctype = 'text/html; charset=utf-8'
+  } else if (url.endsWith('.js')) {
+    ctype = 'application/javascript'
+  } else if (url.endsWith('.mjs')) {
+    ctype = 'application/javascript'
+  } else if (url.endsWith('.css')) {
+    ctype = 'text/css'
+  }
+  let data = null
+  try {
+    data = fs.readFileSync('static' + url)
+  } catch (e) {
+  }
+  res.header('Content-Type', ctype)
+  res.send(data)
+})
+
+app.listen(PORT, () => {
+  console.log('to access the top')
+  console.log(`http://localhost:${PORT}/`)
+  console.log()
+  console.log('edit .env if you want to change')
+  console.log()
+  console.log('https://github.com/code4fukui/push/')
+})
